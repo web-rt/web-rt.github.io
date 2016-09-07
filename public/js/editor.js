2ea08218dfa027f4396fecfd1a37f104fceb1202
liang.fang <liang.fang@zhan.com>
2016-09-07 18:50:19 +0800

    update

---
+++ public/js/editor.js
@@ -0,0 +1,20 @@
+$(function(){
+  // https://github.com/ajaxorg/ace/wiki/Configuring-Ace
+  var editor = ace.edit('blob-editor')
+  editor.setOptions({
+    highlightActiveLine: false
+  })
+  editor.getSession().setOptions({
+    
+    useSoftTabs: true,
+    tabSize: 6
+  })
+  editor.renderer.setOptions({
+    showGutter: false,
+    fontSize: '16px',
+    showPrintMargin: false,
+    theme: 'ace/theme/github',
+    minLines: 24,
+    maxLines: 50
+  });
+})
